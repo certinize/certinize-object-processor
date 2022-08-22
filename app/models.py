9f83d99ada1027108884ee5b5b9cfec605131347
Hju Kneyck Flores <hjucode@gmail.com>
2022-08-22 15:46:19 +0800

    fix: resolve validator checking incorrect key

--- app/models.py
+++ app/models.py
@@ -51,28 +51,28 @@ class CertificateTemplateMeta(pydantic.BaseModel):
     def issuance_date_meta_must_have_valid_structure(cls, value: dict[str, typing.Any]):
         # Check if value has font_size and position keys
         if "font_size" not in value:
-            raise ValueError("recipient_name_meta must have a font_size key")
+            raise ValueError("issuance_date_meta must have a font_size key")
 
         if "position" not in value:
-            raise ValueError("recipient_name_meta must have a position key")
+            raise ValueError("issuance_date_meta must have a position key")
 
         # Check if value font_size is an int
         if not isinstance(value["font_size"], int):
-            raise ValueError("recipient_name_meta font_size must be an int")
+            raise ValueError("issuance_date_meta font_size must be an int")
 
         # Check if value position is a dict with x and y keys
         if "x" not in value["position"]:
-            raise ValueError("recipient_name_meta position must have a x key")
+            raise ValueError("issuance_date_meta position must have a x key")
 
         if "y" not in value["position"]:
-            raise ValueError("recipient_name_meta position must have a y key")
+            raise ValueError("issuance_date_meta position must have a y key")
 
         # Check if value position x and y are integers
         if not isinstance(value["position"]["x"], int):
-            raise ValueError("recipient_name_meta position x must be an int")
+            raise ValueError("issuance_date_meta position x must be an int")
 
         if not isinstance(value["position"]["y"], int):
-            raise ValueError("recipient_name_meta position y must be an int")
+            raise ValueError("issuance_date_meta position y must be an int")
 
         return value
 
@@ -80,15 +80,17 @@ class CertificateTemplateMeta(pydantic.BaseModel):
     @classmethod
     def recipients_must_have_valid_structure(cls, value: list[dict[str, str]]):
         # Check if list contains dicts with name keys
-        for recipient in value:
-            if "name" not in recipient:
-                raise ValueError("recipient must have a name key")
+        for recipients in value:
+            if "recipient_name" not in recipients:
+                raise ValueError("recipients must have a recipient_name key")
 
             # Check that value name does not have a zero length
             # We don't want users sending strings with zero length. They should be
             # marked as spam.
-            if len(recipient["name"]) == 0:
+            if len(recipients["recipient_name"]) == 0:
                 raise ValueError("recipient name must not be empty")
+
+        return value
 
 
 @dataclasses.dataclass
